a81f9162c9781165f4e98c8a3992d3293bc1623d
Avihai <avihaimo1@gmail.com>
2025-04-27 13:52:20 +0300

    no message

--- app/src/main/java/com/havah_avihaim_emanuelm/finderlog/MainActivity.java
+++ app/src/main/java/com/havah_avihaim_emanuelm/finderlog/MainActivity.java
@@ -1,12 +1,12 @@
 package com.havah_avihaim_emanuelm.finderlog;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navbar);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_lost) {
+                startActivity(new Intent(this, LostItemsActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_matches) {
+                startActivity(new Intent(this, MatchesActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_found) {
+                startActivity(new Intent(this, FoundItemsActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_reports) {
+                startActivity(new Intent(this, ReportsActivity.class));
+                return true;
+            }
+            return false;
         });
+
     }
 }
